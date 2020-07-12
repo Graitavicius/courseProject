0e74662996c86451e6e8efa3fb273bd18e02365d
Graitavicius <grait.mindaugas@gmail.com>
2020-07-12 16:37:01 +0300

    trasforming response data, resolving data before loading

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,33 +7,35 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Delicious Hot Dog',
-      'You have to try it!',
-      'https://live.staticflickr.com/4899/32770365848_8f84af509c_b.jpg',
-      [
-        new Ingredient('Buns', 1),
-        new Ingredient('Sausage', 1),
-        new Ingredient('Mustard', 1),
-      ]
-    ),
-    new Recipe(
-      'Tasty Burger',
-      'The best burger on the planet!',
-      'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Beef', 1),
-        new Ingredient('Cheese', 1),
-        new Ingredient('Tomato', 2),
-        new Ingredient('Pickles', 3),
-        new Ingredient('Onion', 1),
-        new Ingredient('Mayonnaise', 1),
-        new Ingredient('Lettuce', 1),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Delicious Hot Dog',
+  //     'You have to try it!',
+  //     'https://live.staticflickr.com/4899/32770365848_8f84af509c_b.jpg',
+  //     [
+  //       new Ingredient('Buns', 1),
+  //       new Ingredient('Sausage', 1),
+  //       new Ingredient('Mustard', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Tasty Burger',
+  //     'The best burger on the planet!',
+  //     'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Beef', 1),
+  //       new Ingredient('Cheese', 1),
+  //       new Ingredient('Tomato', 2),
+  //       new Ingredient('Pickles', 3),
+  //       new Ingredient('Onion', 1),
+  //       new Ingredient('Mayonnaise', 1),
+  //       new Ingredient('Lettuce', 1),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
   setRecipes(recipes: Recipe[]) {
