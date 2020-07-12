5bab1646808b36d81155ef12dfa0049b6255e441
Graitavicius <grait.mindaugas@gmail.com>
2020-07-12 16:06:22 +0300

    added backend(firebase), added DataStorage service, added functionality to save data and fetch data buttons

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -36,6 +36,10 @@ export class RecipeService {
   ];
 
   constructor(private shoppingListService: ShoppingListService) {}
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
