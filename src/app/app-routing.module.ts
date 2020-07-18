243ed0fb0b65cfcdb26b29bf6b28c79d7b116dad
Graitavicius <grait.mindaugas@gmail.com>
2020-07-18 21:44:19 +0300

    added auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
